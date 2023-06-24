f3301aa443954fa9a4368742b5bf407cf99d92fa
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-24 12:06:34 -0700

    string escape sequence

---
+++ codewithmosh/Part5.java
@@ -0,0 +1,22 @@
+package codewithmosh;
+
+public class Part5 {
+    public static void main(String[] args) {
+        // "\to exit"
+        String message = "Hello \"Michael\"";
+        System.out.println(message);
+        System.out.println("\n");
+        
+        // c:\Windows\..s
+        String file = "c:\\Windows\\..";
+        System.out.println(file);
+        
+        String fileN = "c:\n\\Windows\\..";
+        System.out.println("\n");
+        System.out.println(fileN);
+
+        String fileT = "c:\t\\Windows\\..";
+        System.out.println("\n");
+        System.out.println(fileT);
+    }
+}
