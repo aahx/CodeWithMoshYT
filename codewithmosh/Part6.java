4d6ad5880cc27b84ee1cfd2c6a7a5e471552b1dd
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-25 15:07:26 -0700

    save

--- codewithmosh/Part6.java
+++ codewithmosh/Part6.java
@@ -3,6 +3,8 @@ package codewithmosh;
 public class Part6 {
     public static void main(String[] args) {
         int number = 1;
-        
+         
     }
 }
+
+//https://www.youtube.com/watch?v=eIrMbAQSU34
