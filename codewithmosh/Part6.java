7f8d9fe9cc5f182d778e802386dcbbc82c927978
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-25 17:27:12 -0700

    Arrays

--- codewithmosh/Part6.java
+++ codewithmosh/Part6.java
@@ -1,10 +1,18 @@
 package codewithmosh;
 
+import java.util.Arrays;
+
 public class Part6 {
     public static void main(String[] args) {
-        int number = 1;
-         
+        // int[] numbers = new int[5];
+        // numbers[0] = 10;
+
+        // arrays have a fixed length
+
+        int[] numbers = { 2, 3, 5, 1, 4 };
+        Arrays.sort(numbers);
+
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
     }
 }
-
-//https://www.youtube.com/watch?v=eIrMbAQSU34
