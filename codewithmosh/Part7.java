8c2b3719760815e48b6f3c8b9ca5e028378e0e6f
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-25 17:31:17 -0700

    multi-dimensional array

---
+++ codewithmosh/Part7.java
@@ -0,0 +1,25 @@
+package codewithmosh;
+
+import java.util.Arrays;
+
+public class Part7 {
+    public static void main(String[] args) {
+        // // 2 row and 3 columns
+        // int[][] numbers = new int[2][3];
+
+        // System.out.println(numbers);
+        // System.out.println(Arrays.deepToString(numbers));
+
+        // // 3d matrix
+        // int[][][] numbers = new int[2][3][5];
+
+        // System.out.println(numbers);
+        // System.out.println(Arrays.deepToString(numbers));
+
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+        System.out.println(numbers);
+        System.out.println(Arrays.deepToString(numbers));
+
+    }
+}
