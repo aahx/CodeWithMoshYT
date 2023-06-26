13a34df1c4c09ebec17bad45c9c7ab1a863ce515
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-25 18:55:14 -0700

    math class

---
+++ codewithmosh/Part11_MathClass.java
@@ -0,0 +1,25 @@
+// Math Class
+
+package codewithmosh;
+
+public class Part11_MathClass {
+    public static void main(String[] args) {
+        // overloaded - same method name but different parameters
+
+        // int result = Math.round(1.5f);
+        // System.out.println(result);
+
+        // next largest
+        // double result = Math.ceil(1.1f);
+        // int result = (int) Math.ceil(1.1f);
+        
+        // Math.floor - lowest
+        double result = Math.floor(1.1f);
+        System.out.println(result);
+
+        // Math.min
+        // Math.max
+        // Math.abs
+        // Math.random(); 0 to 1
+    }
+}
