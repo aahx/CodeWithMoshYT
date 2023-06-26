65c9dffa989380707a4a903130b17dc5fa25b2a6
Michael Kim <michaelkim@michaels-imac.lan>
2023-06-25 17:33:22 -0700

    final/ constant

---
+++ codewithmosh/Part8.java
@@ -0,0 +1,11 @@
+package codewithmosh;
+
+public class Part8 {
+    public static void main(String[] args) {
+        final float PI = 3.14f;
+        System.out.println(PI);
+        
+
+    }
+    
+}
